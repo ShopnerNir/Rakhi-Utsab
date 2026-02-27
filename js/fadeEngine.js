6c690d15b8b71a70e6552090657a2cc8a9b91afd
ShopnerNir <shopner.nir500@gmail.com>
2026-02-27 20:54:44 +0600

    Update fadeEngine.js

--- js/fadeEngine.js
+++ js/fadeEngine.js
@@ -1,16 +1,19 @@
 const sections=document.querySelectorAll("#allSections section");
 
 function revealSections(){
-const triggerBottom=window.innerHeight*0.85;
+    const triggerBottom=window.innerHeight*0.85;
 
-sections.forEach(section=>{
-const top=section.getBoundingClientRect().top;
+    sections.forEach(section=>{
+        const top=section.getBoundingClientRect().top;
 
-if(top<triggerBottom){
-section.classList.add("active");
+        if(top<triggerBottom){
+            section.classList.add("active");
+        }else{
+            section.classList.remove("active");
+        }
+    });
 }
-});
-}
 
+// Initial check & scroll listener
 window.addEventListener("scroll",revealSections);
-window.addEventListener("load",revealSections);
+window.addEventListener("load",revealSections);;
